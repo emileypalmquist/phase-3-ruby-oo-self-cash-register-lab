2fa66b381965099fa03a78b828674de47a71c6bb
Emiley Palmquist <54187547+emileypalmquist@users.noreply.github.com>
2022-10-13 15:23:39 -0700

    commit tree 195456b57e972fb4ab53ff09f43e7300d310d5a4

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,48 @@
+require 'pry'
 
+class CashRegister
+
+  attr_reader :discount, :items
+  attr_accessor :total, :last_price
+
+  def initialize discount=0
+    @discount = discount
+    @total = 0
+    @items = []
+    @last_price = 0
+  end
+
+  def add_item(title, price, quantity=1)
+    quantity.times do
+      self.items << title
+    end
+    self.last_price= price * quantity
+    # PEMDAS
+    # parenthesis - exponents - multiplication - division - addition - subtraction
+    self.total = self.total + price * quantity
+  end
+
+  def apply_discount
+    if self.discount > 0
+      discount_amount = self.total * self.discount / 100.0
+      self.total = self.total - discount_amount
+      "After the discount, the total comes to $#{self.total.round}."
+    else 
+      "There is no discount to apply."
+    end
+  end
+
+  def void_last_transaction
+    # binding.pry
+    self.total= self.total - self.last_price
+  end
+
+  # def total
+  #   @total
+  # end
+
+  # def total=(total)
+  #   @total = total
+  # end
+
+end
